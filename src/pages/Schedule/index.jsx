0acc2b33010b82ae9b034c13e0308d8fe95bfffa
Sirros <121970263@qq.com>
2020-12-28 13:52:16 +0800

    feature: sechdule page layout

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -1,10 +1,106 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Calendar, Badge, Row, Col,
+  List
+} from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
+import styles from './index.less';
+import moment from 'moment';
 
-export default () => {
-  return (
-    <PageContainer>
-      schedule
-    </PageContainer>
-  );
-};
+class Schedule extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultDate: moment(),
+      selectedDate: moment(),
+      selectedList: []
+    };
+  }
+  // 日期选择回调
+  handleSelect = (value) => {
+    const data = this.getListData(value)
+    this.setState({
+      defaultDate: value,
+      selectedDate: value,
+      selectedList: data
+    })
+  }
+   
+  // 获取数据
+  getListData = (value) => {
+    let listData;
+    switch(value.date()) {
+      case 8:
+        listData = [
+          { type: 'warning', content: 'This is warning event.' },
+          { type: 'success', content: 'This is usual event.' },
+        ];
+      break;
+      case 10:
+        listData = [
+          { type: 'warning', content: 'This is warning event.' },
+          { type: 'success', content: 'This is usual event.' },
+          { type: 'error', content: 'This is error event.' },
+        ];
+        break;
+      case 15:
+        listData = [
+          { type: 'warning', content: 'This is warning event' },
+          { type: 'success', content: 'This is very long usual event.' },
+          { type: 'error', content: 'This is error event 1.' },
+          { type: 'error', content: 'This is error event 2.' },
+          { type: 'error', content: 'This is error event 3.' },
+          { type: 'error', content: 'This is error event 4.' },
+        ];
+        break;
+      default:
+    }
+    return listData || [];
+  }
+
+  // 自定义日期渲染
+  dateCellRender = (value) => {
+    const data = this.getListData(value);
+    return (
+      <ul className={styles.Events}>
+        {
+          data.map(item => (
+            <li key={item.content}>
+              <Badge status={item.type} text={item.content} />
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <PageContainer>
+        <Row gutter={5} className={styles.Wrapper}>
+          <Col span={9}>
+            当前选择日期：{this.state.selectedDate.format('YYYY-MM-DD')}
+            <List>
+              {
+                this.state.selectedList.map(item => (
+                  <List.Item key={item.content}>
+                    {item.content}
+                  </List.Item>
+                ))
+              }
+            </List>
+          </Col>
+          <Col span={15} style={{borderLeft: '1px solid #e4e4e4'}}>
+            <Calendar
+              value={this.state.defaultDate}
+              onSelect={this.handleSelect}
+              dateCellRender={this.dateCellRender}
+            />
+          </Col>
+        </Row>
+      </PageContainer>
+    );
+  }
+}
+
+export default Schedule;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,123 +1,154 @@
-import React from 'react';
+import React, { Component, forwardRef } from 'react';
 import { Card } from 'antd';
 import { FormattedMessage } from 'umi';
 import {
   Row, Col, Carousel, Statistic,
+  Divider, List
 } from 'antd';
 import {
   LikeOutlined,
   ArrowUpOutlined,
-  ArrowDownOutlined,
+  AlertOutlined,
 } from '@ant-design/icons';
 import TeamLogo from '../assets/logo.png';
 import styles from './Welcome.less';
 
 const { Meta } = Card;
 
-export default () => {
-  return (
-    <div className={styles.homeTopWrapper}>
-      <div className={styles.homeTopLeft}>
-        <Card>
-          <img src={TeamLogo} alt="logo" />
-          <Meta title="title" description="xxxxxxx" style={{ marginTop: 15 }} />
-        </Card>
-        <Row>
-          <Col span={24}>
-            <Card title="组织信息" hoverable={true}>
-              xxxxxxxxxxxxxxxxx
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Card hoverable={true}>
-              <Statistic title='团队经费' value={1128} prefix={<LikeOutlined />} />
-            </Card>
-          </Col>
-        </Row>
-      </div>
-      <div className={styles.homeTopMid}>
-        <Row justify="center" gutter={10}>
-          <Col span={12}>
-            <Card hoverable={true}>
-              运动员
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              管理员
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              即将到来的比赛
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              照片
-              <br />
-              #10
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Card hoverable={true}>
-              公告
-              <Meta description="asofhaghasgasasfasfasfasfasfasfasfasfasfasfas" />
-            </Card>
-          </Col>
-        </Row>
-      </div>
-      <div className={styles.homeTopRight}>
-        <Carousel style={{marginBottom: 10}} autoplay dotPosition="bottom">
-          <div className={styles.carouselItem}>
-            <img
-              src="https://images.pexels.com/photos/5168816/pexels-photo-5168816.jpeg?cs=srgb&dl=pexels-alex-kozlov-5168816.jpg&fm=jpg"
-              alt=""
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+export default class extends Component {
+  render () {
+    return (
+      <div className={styles.homeTopWrapper}>
+        <div className={styles.homeTopLeft}>
+          <Card>
+            <img src={TeamLogo} alt="logo" />
+            <Meta title="title" description="xxxxxxx" style={{ marginTop: 15 }} />
+          </Card>
+          {/* <Row>
+            <Col span={24}>
+              <Card style={{color: '#c4c4c4'}} hoverable={true}>
+                所属：四川大学软件学院  
+              </Card>
+            </Col>
+          </Row> */}
+          <Row>
+            <Col span={24}>
+              <Card hoverable={true}>
+                <Statistic title='团队经费' value={1128} prefix={<LikeOutlined />} />
+              </Card>
+            </Col>
+          </Row>
+        </div>
+        <div className={styles.homeTopMid}>
+          <Row justify="center" gutter={10}>
+            <Col span={12}>
+              <Card hoverable={true}>
+                运动员
+                <br />
+                #10
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card hoverable={true}>
+                管理员
+                <br />
+                #10
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card hoverable={true}>
+                即将到来的比赛
+                <br />
+                #10
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card hoverable={true}>
+                照片
+                <br />
+                #10
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Card hoverable={true}>
+                <AlertOutlined />公告
+                <List
+                  className={styles.listText}
+                  size="small"
+                  bordered
+                  dataSource={data}
+                  renderItem={item => <List.Item>{item}</List.Item>}
+                />
+              </Card>
+            </Col>
+          </Row>
+        </div>
+        <div className={styles.homeTopRight}>
+          <Carousel style={{marginBottom: 10}} autoplay dotPosition="bottom">
+            <div className={styles.carouselItem}>
+              <img
+                src="https://images.pexels.com/photos/5168816/pexels-photo-5168816.jpeg?cs=srgb&dl=pexels-alex-kozlov-5168816.jpg&fm=jpg"
+                alt=""
+              />
+            </div>
+            <div className={styles.carouselItem}>
+              <img
+                src="https://images.pexels.com/photos/1842580/pexels-photo-1842580.jpeg?cs=srgb&dl=pexels-matt-hardy-1842580.jpg&fm=jpg"
+                alt=""
+              />
+            </div>
+          </Carousel>
+          <Row gutter={10}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="今日气温"
+                  value={21.28}
+                  precision={2}
+                  valueStyle={{ color: '#3f8600' }}
+                  suffix="%"
+                />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="明日气温"
+                  value={23.4}
+                  precision={2}
+                  valueStyle={{ color: '#3f8600' }}
+                  prefix={<ArrowUpOutlined />}
+                  suffix="%"
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Divider dashed className={styles.myText}>想对自己说</Divider>
+          <div>
+            <List
+              onMouseOver={() => {console.log('over')}}
+              className={styles.listText}
+              size="small"
+              bordered
+              dataSource={data}
+              renderItem={item => <List.Item>{item}</List.Item>}
             />
           </div>
-          <div className={styles.carouselItem}>
-            <img
-              src="https://images.pexels.com/photos/1842580/pexels-photo-1842580.jpeg?cs=srgb&dl=pexels-matt-hardy-1842580.jpg&fm=jpg"
-              alt=""
-            />
-          </div>
-        </Carousel>
-        <Row gutter={10}>
-          <Col span={12}>
-            <Card>
-              <Statistic
-                title="今日气温"
-                value={21.28}
-                precision={2}
-                valueStyle={{ color: '#3f8600' }}
-                suffix="%"
-              />
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card>
-              <Statistic
-                title="明日气温"
-                value={23.4}
-                precision={2}
-                valueStyle={{ color: '#3f8600' }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-              />
-            </Card>
-          </Col>
-        </Row>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
