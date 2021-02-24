5b312a5cb6d77fb1933e8425723a6eb21ec25785
Sirros <121970263@qq.com>
2021-02-24 18:05:21 +0800

    refactor: schedule class component to func component

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import {
-  Calendar, Badge, Row, Col,
-  List, Divider
-} from 'antd';
+import React, { useState } from 'react';
+import { Calendar, Badge, Row, Col, List, Divider } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import styles from './index.less';
 import moment from 'moment';
 
-class Schedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultDate: moment(),
-      selectedDate: moment(),
-      selectedList: []
-    };
-  }
+export default () => {
+  const [defaultDate, setDefaultDate] = useState(moment());
+  const [selectedDate, setSelectedDate] = useState(moment());
+  const [selectedList, setSelectedList] = useState([]);
+  const [panelMode, setPanelMode] = useState('month');
+
   // 日期选择回调
-  handleSelect = (value) => {
-    const data = this.getListData(value)
-    this.setState({
-      defaultDate: value,
-      selectedDate: value,
-      selectedList: data
-    })
-  }
-   
+  const handleSelect = (value) => {
+    const data = getListData(value);
+    setDefaultDate(value);
+    setSelectedDate(value);
+    setSelectedList(data);
+  };
+
+  const handlePanelChange = (moment, mode) => {
+    setPanelMode(mode);
+  };
+
   // 获取数据
-  getListData = (value) => {
+  const getListData = (value) => {
     let listData;
-    switch(value.date()) {
+    switch (value.date()) {
       case 8:
-        if(value.month() === 9) {
+        if (value.month() === 9) {
           listData = [
             { type: 'warning', content: 'kkp.' },
             { type: 'success', content: 'sudu kkp' },
           ];
         }
-      break;
+        break;
       case 10:
         listData = [
           { type: 'warning', content: 'This is warning event.' },
@@ -58,81 +54,76 @@ class Schedule extends Component {
       default:
     }
     return listData || [];
-  }
+  };
 
   // 自定义日期渲染
-  dateCellRender = (value) => {
-    const data = this.getListData(value);
+  const dateCellRender = (value) => {
+    const data = getListData(value);
     return (
       <ul className={styles.Events}>
-        {
-          data.map(item => (
-            <li key={item.content}>
-              <Badge status={item.type} text={item.content} />
-            </li>
-          ))
-        }
+        {data.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
       </ul>
-    )
-  }
+    );
+  };
 
   // day change func
-  changeDay = (day) => {
-    switch(day.day()) {
+  const changeDay = (day) => {
+    switch (day.day()) {
       case 1:
         return '一';
         break;
       case 2:
         return '二';
-       break;
+        break;
       case 3:
         return '三';
-       break;
+        break;
       case 4:
         return '四';
-       break;
+        break;
       case 5:
         return '五';
-       break;
+        break;
       case 6:
         return '六';
-       break;
+        break;
       default:
         return '日';
     }
-  }
+  };
 
-  render() {
-    return (
-      <PageContainer>
-        <Row gutter={5} className={styles.Wrapper}>
-          <Col span={11} className={styles.leftPart} style={{padding: 15}}>
-            <p>
-              <span style={{marginRight: 10}}>{this.state.selectedDate.format('YYYY-MM-DD')}</span>
-              <span style={{marginLeft: 10}}>星期：{this.changeDay(this.state.selectedDate)}</span>
-            </p>
-            <Divider dashed/>
-            <List>
-              {
-                this.state.selectedList.map(item => (
-                  <List.Item key={item.content} style={{fontSize: 16}}>
-                    <Badge status={item.type} /> {item.content}
-                  </List.Item>
-                ))
-              }
-            </List>
-          </Col>
-          <Col span={13} style={{borderLeft: '1px solid #e4e4e4'}}>
-            <Calendar
-              value={this.state.defaultDate}
-              onSelect={this.handleSelect}
-              dateCellRender={this.dateCellRender}
-            />
-          </Col>
-        </Row>
-      </PageContainer>
-    );
-  }
-}
-
-export default Schedule;
+  return (
+    <PageContainer>
+      <Row gutter={5} className={styles.Wrapper}>
+        <Col span={11} className={styles.leftPart} style={{ padding: 15 }}>
+          <p>
+            <span style={{ marginRight: 10 }}>{selectedDate.format('YYYY-MM-DD')}</span>
+            {panelMode === 'month' && (
+              <span style={{ marginLeft: 10 }}>星期：{changeDay(selectedDate)}</span>
+            )}
+          </p>
+          <Divider dashed />
+          <List>
+            {selectedList.map((item) => (
+              <List.Item key={item.content} style={{ fontSize: 16 }}>
+                <Badge status={item.type} /> {item.content}
+              </List.Item>
+            ))}
+          </List>
+        </Col>
+        <Col span={13} style={{ borderLeft: '1px solid #e4e4e4' }}>
+          <Calendar
+            value={defaultDate}
+            onSelect={handleSelect}
+            dateCellRender={dateCellRender}
+            onPanelChange={handlePanelChange}
+          />
+        </Col>
+      </Row>
+    </PageContainer>
+  );
+};
