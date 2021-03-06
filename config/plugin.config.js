78d6a09e29dbd049cc4f7781f7cc5fedb5f5e3ed
Sirros <121970263@qq.com>
2021-03-06 18:58:39 +0800

    feat:home page layout update

---
+++ config/plugin.config.js
@@ -0,0 +1,5 @@
+import path from 'path';
+
+default export config = () => {
+    
+}
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -57,144 +57,128 @@ export default () => {
   return (
     <div className={styles.homeTopWrapper}>
       <div className={styles.homeTopLeft}>
-        <Card>
-          <img src={TeamLogo} alt="logo" />
-          <Meta title="title" description="xxxxxxx" style={{ marginTop: 15 }} />
-        </Card>
-        {/* <Row>
-            <Col span={24}>
-              <Card style={{color: '#c4c4c4'}} hoverable={true}>
-                所属：四川大学软件学院  
-              </Card>
-            </Col>
-          </Row> */}
-        <Row>
-          <Col span={24}>
-            <Card hoverable={true}>
-              <Statistic title="团队经费" value={1128} prefix={<LikeOutlined />} />
-            </Card>
-          </Col>
-        </Row>
+        <div className={styles.leftTop}>
+          <Card>
+            <img src={TeamLogo} alt="logo" />
+            <Meta title="title" description="xxxxxxx" style={{ marginTop: 15 }} />
+          </Card>
+        </div>
+        <div className={styles.leftBottom}>
+          <Card hoverable={true}>
+            <Statistic title="团队经费" value={1128} prefix={<LikeOutlined />} />
+          </Card>
+        </div>
       </div>
       <div className={styles.homeTopMid}>
-        <Row justify="center" gutter={10}>
-          <Col span={12}>
-            <Card hoverable={true}>
-              运动员
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              管理员
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              即将到来的比赛
-              <br />
-              #10
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card hoverable={true}>
-              照片
-              <br />
-              #10
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Card hoverable={true}>
-              <Badge dot>
-                <NotificationOutlined />
-              </Badge>
-              <span style={{ marginLeft: 5 }}>公告</span>
-              <List
-                className={styles.listText}
-                size="small"
-                bordered
-                dataSource={data}
-                renderItem={(item) => <List.Item>{item}</List.Item>}
-              />
-            </Card>
-          </Col>
-        </Row>
+        <div className={styles.midTop}>
+          <Row justify="center" gutter={10}>
+            <div className={styles.firstRow}>
+              <Card hoverable={true}>
+                运动员
+                <br />
+                #10
+              </Card>
+              <Card hoverable={true}>
+                管理员
+                <br />
+                #10
+              </Card>
+            </div>
+            <div className={styles.secondRow}>
+              <Card hoverable={true}>
+                即将到来的比赛
+                <br />
+                #10
+              </Card>
+              <Card hoverable={true}>
+                照片
+                <br />
+                #10
+              </Card>
+            </div>
+          </Row>
+        </div>
+        <div className={styles.midBottom}>
+          <Card hoverable={true}>
+            <Badge dot>
+              <NotificationOutlined />
+            </Badge>
+            <span style={{ marginLeft: 5 }}>公告</span>
+            <List
+              className={styles.listText}
+              size="small"
+              bordered
+              dataSource={data}
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+            />
+          </Card>
+        </div>
       </div>
       <div className={styles.homeTopRight}>
-        <Carousel style={{ marginBottom: 10 }} autoplay dotPosition="bottom">
-          <div className={styles.carouselItem}>
-            <img
-              src="https://images.pexels.com/photos/5168816/pexels-photo-5168816.jpeg?cs=srgb&dl=pexels-alex-kozlov-5168816.jpg&fm=jpg"
-              alt=""
-            />
-          </div>
-          <div className={styles.carouselItem}>
-            <img
-              src="https://images.pexels.com/photos/1842580/pexels-photo-1842580.jpeg?cs=srgb&dl=pexels-matt-hardy-1842580.jpg&fm=jpg"
-              alt=""
-            />
-          </div>
-        </Carousel>
-        <Row gutter={10}>
-          <Col span={12}>
-            <Card>
-              <Statistic
-                title="今日气温"
-                value={21.28}
-                precision={2}
-                valueStyle={{ color: '#3f8600' }}
-                suffix="%"
+        <div className={styles.rightOne}>
+          <Carousel style={{ marginBottom: 10 }} autoplay dotPosition="bottom">
+            <div className={styles.carouselItem}>
+              <img
+                src="https://images.pexels.com/photos/5168816/pexels-photo-5168816.jpeg?cs=srgb&dl=pexels-alex-kozlov-5168816.jpg&fm=jpg"
+                alt=""
               />
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card>
-              <Statistic
-                title="明日气温"
-                value={23.4}
-                precision={2}
-                valueStyle={{ color: '#3f8600' }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Divider dashed className={styles.myText}>
-          想对自己说
-        </Divider>
-        <div>
-          {/* <List
-            onMouseOver={() => {
-              console.log('over');
-            }}
-            onMouseLeave={() => {
-              console.log('leave');
-            }}
-            className={styles.listText}
-            size="small"
-            bordered
-            dataSource={data}
-            renderItem={(item) => <List.Item>{item}</List.Item>}
-          /> */}
-          <div className={styles.parent} ref={warper}>
-            <div className={styles.child} ref={childDom1}>
-              {list.map((item) => (
-                <li
-                  key={item}
-                  onMouseOver={() => hoverHandler(false)}
-                  onMouseLeave={() => hoverHandler(true)}
-                >
-                  {item}
-                </li>
-              ))}
             </div>
-            <div className={styles.child} ref={childDom2}></div>
+            <div className={styles.carouselItem}>
+              <img
+                src="https://images.pexels.com/photos/1842580/pexels-photo-1842580.jpeg?cs=srgb&dl=pexels-matt-hardy-1842580.jpg&fm=jpg"
+                alt=""
+              />
+            </div>
+          </Carousel>
+        </div>
+        <div className={styles.rightTwo}>
+          <div className={styles.weather}>
+            <Row gutter={10}>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="今日气温"
+                    value={21.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="明日气温"
+                    value={23.4}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+            </Row>
+          </div>
+          <div className={styles.personSay}>
+            <Divider dashed className={styles.myText}>
+              队员留言
+            </Divider>
+          </div>
+          <div className={styles.runList}>
+            <div className={styles.parent} ref={warper}>
+              <div className={styles.child} ref={childDom1}>
+                {list.map((item) => (
+                  <li
+                    key={item}
+                    onMouseOver={() => hoverHandler(false)}
+                    onMouseLeave={() => hoverHandler(true)}
+                  >
+                    {item}
+                  </li>
+                ))}
+              </div>
+              <div className={styles.child} ref={childDom2}></div>
+            </div>
           </div>
         </div>
       </div>
