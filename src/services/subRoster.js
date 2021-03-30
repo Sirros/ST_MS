98acca52d7711f7186a5661c9db9ef12701b7501
Sirros <121970263@qq.com>
2021-03-30 14:13:42 +0800

    update: project clean

--- src/services/subRoster.js
+++ src/services/subRoster.js
@@ -19,10 +19,3 @@ export async function addUser(params) {
     data: params,
   });
 }
-
-// export async function updateTeamInfo(params) {
-//   return request('/api/updateTeamInfo', {
-//     method: 'POST',
-//     data: params,
-//   });
-// }
